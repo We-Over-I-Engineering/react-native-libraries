4abb630e61af40d5dda66154e7bfc9e583e0da40
alliya-f <97216007+alliya-f@users.noreply.github.com>
2023-09-13 13:31:57 +0500

    package.json updated

--- Libraries/index.js
+++ Libraries/index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+export {default as CapsuleButton} from './Buttons/CapsuleButton/index';
+export {default as CircleButton} from './Buttons/CircleButton/index';
+export {default as GradientButton} from './Buttons/GradientButton/index';
+export {default as GradientIconButton} from './Buttons/GradientIconButton/index';
+export {default as LoaderButton} from './Buttons/LoaderButton/index';
+export {default as ParallelogramButton} from './Buttons/ParallelogramButton/index';
+export {default as RectangleButton} from './Buttons/RectangleButton/index';
+
 AppRegistry.registerComponent(appName, () => App);
